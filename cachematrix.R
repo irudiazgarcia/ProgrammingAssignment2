d32584920b7fdec3e61a296399e3dcc0c5dcb3bf
irudiazgarcia <irudiaz@ucm.es>
2022-07-11 18:09:34 +0200

    Fully functioning, create a matrix by, for example:
    > matr <- makeCacheMatrix(matrix(c(4,8,3,2,3,5,7,8,9), ncol= 3, nrow = 3))
    
    To find (and create the cache) the inverse, try:
    > cacheSolve(matr)
    
    Retry to use the cache.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,44 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
 
+    set <- function(y){
+      x <<- y
+      m <<- NULL
+    }
+    
+    get <- function() x
+    setinverse <- function(inv) m <<- inv
+    getinverse <- function() m
+     
+    print(list(set = set, get = get, setinverse = setinverse, getinverse = getinverse))
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  data = matrix()
         ## Return a matrix that is the inverse of 'x'
+  
+      if(!(all(is.na(x$getinverse())))){
+        print("getting cached data")
+        return(x$getinverse())
+      }
+    
+    
+    else{
+
+      inv = solve(x$get(), ...)
+      
+      x$setinverse(inv)
+      
+      inv
+      
+    }      
+    
 }
+
+
+
+
